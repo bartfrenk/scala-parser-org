5f7d807e59b76cf6eb5509efdee8b84feeac2fe8
Bart Frenk <bart.frenk@gmail.com>
2016-01-07 13:39:26 +0100

    IN PROGRESS: fix tests

--- src/test/scala/parsers/orgsuite.scala
+++ src/test/scala/parsers/orgsuite.scala
@@ -6,19 +6,27 @@ import org.scalacheck.Gen
 
 import scala.language.postfixOps
 
-import OrgParsers._
 
 class OrgFunSuite extends FunSuite with PropertyChecks with Matchers {
 
-  test("Parse header correctly") {
+  import OrgParsers._
+  import OrgNode._
+
+  def runT[A](p: OrgParser[A])(input: String) = OrgParsers.run(p)(input) match {
+    case Success(a) => a
+    case Failure(_) => throw new Exception()
+  }
+
+  test("Parse header") {
     forAll(Gen.choose(1, 10), Gen.alphaStr) {(count: Int, text: String) => {
       val input = "*" * count + " " + text + "\n"
-      val result = run(header)(input)
-      result.level should equal (level)
-      result.content should equal (text, EOL)
+      val result = runT(header)(input)
+      result.level should equal (count)
+      result.content should equal ((text, OrgNode.EOL))
     }}
   }
 
+  /* Need to adapt to new Line type. */
   /*
   test("Parse paragraph correctly") {
     val strings: List[String] = List(
